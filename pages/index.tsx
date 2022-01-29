40344d53b5633c1a98cd0d844e8d4a655ab826e7
Zakiego <belummikir@gmail.com>
2022-01-29 16:49:56 +0800

    stye: redesign index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,81 +1,44 @@
 import Head from "next/head";
 import Image from "next/image";
 
+import Icon from "~/public/next-tailwind.svg";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="vercel flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-12 text-center md:px-20">
+        <Image
+          src={Icon}
+          height={100}
+          width={400}
+          alt="Next.js + Tailwind CSS"
+        />
+
+        <h1 className="mt-5 text-2xl font-bold md:text-5xl">
+          <a href="https://nextjs.org/">Next.js</a> and{" "}
+          <a
+            className="border-b-2 border-transparent text-[#06B6D4]  transition-all duration-500 hover:border-b-2 hover:border-[#06B6D4]"
+            href="https://nextjs.org"
+          >
+            Tailwind CSS
           </a>
         </h1>
 
-        <div className="mt-5 text-2xl font-medium">
-          NextJS with TypeScript and Tailwind CSS
+        <div className="mt-4 text-base opacity-40 md:text-2xl">
+          With TypeSciprt, Jest, Prettier, and ESLint
         </div>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{" "}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.js
-          </code>
-        </p>
 
         <a
           href="https://github.com/zakiego/nextjs-typescript-tailwindcss-starter"
-          className="mt-7 border-b-2 border-transparent text-blue-600 transition-all duration-500 hover:border-b-2 hover:border-blue-600"
+          className="mt-7 border-b-2 border-transparent text-[#06B6D4] transition-all duration-500 hover:border-b-2 hover:border-[#06B6D4]"
         >
           view github repository
         </a>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
