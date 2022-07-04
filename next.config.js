16eb6478a2d672b8d462cd4eff167736f3d5fc1c
Zakiego <zakiyuddinmunziri@gmail.com>
2022-07-04 12:37:54 +0000

    next: add swc minify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true, // https://nextjs.org/docs/advanced-features/compiler#minification
 };
 
 module.exports = nextConfig;
